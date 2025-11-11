5b42f0f24465f7a0d884e687c12ec1ba10297bc4
Deepesh-Katudia <deepeshkatudia6201@gmail.com>
2025-11-11 14:43:40 -0500

    Completed : 1. Component + Life Cycle Events

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Admin } from './component/admin/admin';
+import { User } from './component/user/user';
+import { DataBinding } from "./data-binding/data-binding";
+import { Signal } from './component/signal/signal';
+import { ControlFlow } from './component/control-flow/control-flow';
+import { Attribute } from "./component/attribute/attribute";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Admin, User, DataBinding, Signal, ControlFlow, Attribute],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
